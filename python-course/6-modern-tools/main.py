25adbe3584c6ec7b7bef3582dcbe3d7ba343c01b
Brian <braisuhit@gmail.com>
2026-01-05 21:22:58 -0300

    feat(course): agregar modulo 6 de herramientas modernas uv y jupyter interactive

---
+++ python-course/6-modern-tools/main.py
@@ -0,0 +1,10 @@
+# %%
+# (Ese símbolo '# %%' crea una celda interactiva en un archivo normal)
+print("Bloque 1: Configuración")
+variable_x = 10
+variable_y = 20
+
+# %%
+print("Bloque 2: Cálculo")
+resultado = variable_x + variable_y
+print(f"El resultado es {resultado}")
